efc885b763df37a342b7067f88b042c422ee78e9
Shelby Lem <slem@nd.edu>
2018-04-04 22:37:01 -0400

    Initial commit

---
+++ Food Stand Inventory App/StartSellingController.swift
@@ -0,0 +1,21 @@
+//
+//  StartSellingController.swift
+//  Food Stand Inventory App
+//
+//  Created by sally everlove on 4/4/18.
+//  Copyright © 2018 Shelby Lem. All rights reserved.
+//
+
+import UIKit
+
+class StartSellingController: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
